4847ad875305333185affa0105243cb9df5ac0da
David Bissaco da Silva <davidbissacodasilva@gmail.com>
2025-02-04 21:35:23 -0300

    Footer completed

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Header from './Components/Header/Header';
 import Sidebar from './Components/Sidebar/Sidebar';
 import Main from './Components/Main/Main';
+import Footer from './Components/Footer/Footer';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Sidebar />
       <Header />
       <Main/>
+      <Footer />
     </div>
   );
 }
