97c17f3070b1a89a5b99498a43cd1877163f795c
David Bissaco da Silva <davidbissacodasilva@gmail.com>
2025-02-04 21:11:16 -0300

    Sidebar completed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import Header from './Components/Header/Header';
+import Sidebar from './Components/Sidebar/Sidebar';
 
 function App() {
   return (
     <div>
+      <Sidebar />
       <Header />
     </div>
   );
