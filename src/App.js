efab090f64ba5b817037a1ef32f0ed67c8e4c132
David Bissaco da Silva <davidbissacodasilva@gmail.com>
2025-02-04 21:26:16 -0300

    Main content

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Header from './Components/Header/Header';
 import Sidebar from './Components/Sidebar/Sidebar';
+import Main from './Components/Main/Main';
 
 function App() {
   return (
     <div>
       <Sidebar />
       <Header />
+      <Main/>
     </div>
   );
 }
